834074d40b237c6985d4cab4571954d54ff89544
Ismael Mariscal <ismael.valladares@cymatic.io>
2022-06-23 10:52:52 -0500

    update on img urls

--- src/components/Layout/components/Sidenav/components/Team/data.items.ts
+++ src/components/Layout/components/Sidenav/components/Team/data.items.ts
@@ -8,21 +8,21 @@ export const items: Item[] = [
   {
     href: '#team-1',
     label: 'Jake Milos',
-    src: 'https://github.com/ismael01866/assets/blob/master/avatar.1.jpg?raw=true'
+    src: 'https://raw.githubusercontent.com/ismael01866/assets/master/avatar.1.jpg'
   },
   {
     href: '#team-2',
     label: 'Andrea Codgins',
-    src: 'https://github.com/ismael01866/assets/blob/master/avatar.2.jpg?raw=true'
+    src: 'https://raw.githubusercontent.com/ismael01866/assets/master/avatar.2.jpg'
   },
   {
     href: '#team-3',
     label: 'Sarah Wilkins',
-    src: 'https://github.com/ismael01866/assets/blob/master/avatar.3.jpg?raw=true'
+    src: 'https://raw.githubusercontent.com/ismael01866/assets/master/avatar.3.jpg'
   },
   {
     href: '#team-4',
     label: 'Jason Alexander',
-    src: 'https://github.com/ismael01866/assets/blob/master/avatar.4.jpg?raw=true'
+    src: 'https://raw.githubusercontent.com/ismael01866/assets/master/avatar.4.jpg'
   }
 ];
